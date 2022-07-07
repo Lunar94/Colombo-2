1470504626d652632c3fea4d5ef7d31d08c2cfcb
Lunar94 <yannoel94@gmail.com>
2022-07-07 13:34:13 -0400

    Build A45.4

--- ui.js
+++ ui.js
@@ -11,14 +11,12 @@ const moreButtonIcon = document.getElementById("btn-more-icon");
 
 // Effet de suppression console
 btnClear.addEventListener("click", () => {
-
+  if (document.querySelector(".response").textContent != ``) sfxDeleteConsole();
 
   document.querySelector(".response").classList.add("animate__flash");
-  setTimeout(function () {
-    document.querySelector(".response").classList.remove("animate__flash");
-  }, 1000);
 
-  setTimeout(function () {
+  setTimeout(() => {
+    document.querySelector(".response").classList.remove("animate__flash");
     document.querySelector(".response").textContent = ``;
   }, 1000);
 });
@@ -51,10 +49,8 @@ function consoleEnlarge() {
   if (consoleCAA.style.height === "100%") {
     consoleCAA.classList.add("animate__shakeY");
     consoleCAA.classList.remove("console-overflow");
-    setTimeout(function () {
+    setTimeout(() => {
       consoleCAA.classList.add("console-overflow");
-    }, 1000);
-    setTimeout(function () {
       consoleCAA.classList.remove("animate__shakeY");
     }, 1000);
     consoleCAA.style.height = "50%";
@@ -65,10 +61,8 @@ function consoleEnlarge() {
     consoleCAA.style.height = "100%";
     consoleCAA.classList.add("animate__slideInUp");
     consoleCAA.classList.remove("console-overflow");
-    setTimeout(function () {
+    setTimeout(() => {
       consoleCAA.classList.add("console-overflow");
-    }, 1000);
-    setTimeout(function () {
       consoleCAA.classList.remove("animate__slideInUp");
     }, 1000);
     moreButtonIcon.classList.remove("fa-expand");
@@ -86,17 +80,7 @@ const paramPanel = document.getElementById("param");
 const paramButtonIcon = document.getElementById("btn-param-icon");
 
 const root = document.querySelector(":root");
-const nuanceDefault = document.getElementById("nuance-default");
-const nuance_1 = document.getElementById("nuance-1");
-const nuance_2 = document.getElementById("nuance-2");
-const nuance_3 = document.getElementById("nuance-3");
-const nuance_4 = document.getElementById("nuance-4");
-const nuance_5 = document.getElementById("nuance-5");
-const nuance_6 = document.getElementById("nuance-6");
-const nuance_7 = document.getElementById("nuance-7");
-const nuance_8 = document.getElementById("nuance-8");
-const nuance_9 = document.getElementById("nuance-9");
-const nuance_10 = document.getElementById("nuance-10");
+const nuanceDefault = document.getElementById("nuanceDefault");
 
 const icons = document.querySelectorAll(".fa-4x");
 
@@ -166,267 +150,100 @@ b2.addEventListener("click", () => {
 // Les nuances
 nuanceDefault.classList.add("colorJS");
 
-nuanceDefault.addEventListener("click", () => {
-  nuanceDefault.classList.add("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N0 (default)
-  root.style.setProperty("--Theme1-Color3", "#F78361");
-  root.style.setProperty("--Theme1-Color4", "#9C523C");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
+const colors = {
+  nuanceDefault: {
+    color1: "#F78361",
+    color2: "#9C523C",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance1: {
+    color1: "#DF6C7F",
+    color2: "#944855",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance2: {
+    color1: "#B06392",
+    color2: "#7D4868",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance3: {
+    color1: "#785F90",
+    color2: "#5D4B6F",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance4: {
+    color1: "#485679",
+    color2: "#3F4B68",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance5: {
+    color1: "#2F4858",
+    color2: "#596D79",
+    color3: "white",
+    color4: "white",
+    color5: "white",
+  },
+  nuance6: {
+    color1: "#1C6E7D",
+    color2: "#2798ad",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance7: {
+    color1: "#039590",
+    color2: "#04c9c2",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance8: {
+    color1: "#4BBC8E",
+    color2: "#378a68",
+    color3: "white",
+    color4: "white",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance9: {
+    color1: "#9BDE7E",
+    color2: "#7bb063",
+    color3: "#334b28",
+    color4: "#334b28",
+    color5: "var(--Theme1-Color3)",
+  },
+  nuance10: {
+    color1: "#F9F871",
+    color2: "#bbba54",
+    color3: "#4a4a20",
+    color4: "#4a4a20",
+    color5: "var(--Theme1-Color3)",
+  },
+};
 
-nuance_1.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.add("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N1
-  root.style.setProperty("--Theme1-Color3", "#DF6C7F");
-  root.style.setProperty("--Theme1-Color4", "#944855");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
+let lastSelected = nuanceDefault;
 
-nuance_2.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.add("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N2
-  root.style.setProperty("--Theme1-Color3", "#B06392");
-  root.style.setProperty("--Theme1-Color4", "#7D4868");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_3.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.add("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N3
-  root.style.setProperty("--Theme1-Color3", "#785F90");
-  root.style.setProperty("--Theme1-Color4", "#5D4B6F");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_4.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.add("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N4
-  root.style.setProperty("--Theme1-Color3", "#485679");
-  root.style.setProperty("--Theme1-Color4", "#3F4B68");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_5.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.add("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N5
-  root.style.setProperty("--Theme1-Color3", "#2F4858");
-  root.style.setProperty("--Theme1-Color4", "#596D79");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "white");
-  // cDefault = 1;
-  // cGrey = 0;
-  // cWhite = 0;
-  // cBlack = 0;
-  // nuanceConsoleDefault.classList.remove("colorJS");
-  // nuanceConsoleGrey.classList.remove("colorJS");
-  // nuanceConsoleWhite.classList.add("colorJS-white");
-  // nuanceConsoleBlack.classList.remove("colorJS");
-
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_6.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.add("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N6
-  root.style.setProperty("--Theme1-Color3", "#1C6E7D");
-  root.style.setProperty("--Theme1-Color4", "#2798ad");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_7.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.add("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N7
-  root.style.setProperty("--Theme1-Color3", "#039590");
-  root.style.setProperty("--Theme1-Color4", "#04c9c2");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_8.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.add("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N8
-  root.style.setProperty("--Theme1-Color3", "#4BBC8E");
-  root.style.setProperty("--Theme1-Color4", "#378a68");
-  root.style.setProperty("--Theme1-Color5", "white");
-  root.style.setProperty("--Selection-Color", "white");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "white");
-  });
-});
-
-nuance_9.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.add("colorJS");
-  nuance_10.classList.remove("colorJS");
-  //N9
-  root.style.setProperty("--Theme1-Color3", "#9BDE7E");
-  root.style.setProperty("--Theme1-Color4", "#7bb063");
-  root.style.setProperty("--Theme1-Color5", "#334b28");
-  root.style.setProperty("--Selection-Color", "#334b28");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "#507340");
-  });
-});
-
-nuance_10.addEventListener("click", () => {
-  nuanceDefault.classList.remove("colorJS");
-  nuance_1.classList.remove("colorJS");
-  nuance_2.classList.remove("colorJS");
-  nuance_3.classList.remove("colorJS");
-  nuance_4.classList.remove("colorJS");
-  nuance_5.classList.remove("colorJS");
-  nuance_6.classList.remove("colorJS");
-  nuance_7.classList.remove("colorJS");
-  nuance_8.classList.remove("colorJS");
-  nuance_9.classList.remove("colorJS");
-  nuance_10.classList.add("colorJS");
-  //N10
-  root.style.setProperty("--Theme1-Color3", "#F9F871");
-  root.style.setProperty("--Theme1-Color4", "#bbba54");
-  root.style.setProperty("--Theme1-Color5", "#4a4a20");
-  root.style.setProperty("--Selection-Color", "#4a4a20");
-  root.style.setProperty("--little-buttons", "var(--Theme1-Color3)");
-
-  icons.forEach((icons) => {
-    icons.style.setProperty("color", "#797836");
-  });
+const testing = document.getElementById("colors-container");
+testing.addEventListener("click", (ev) => {
+  if (ev.target.id == "colors-container") return;
+  lastSelected.classList.remove("colorJS");
+  ev.target.classList.add("colorJS");
+  lastSelected = ev.target;
+  console.log(ev.target.id);
+  root.style.setProperty("--Theme1-Color3", colors[ev.target.id].color1);
+  root.style.setProperty("--Theme1-Color4", colors[ev.target.id].color2);
+  root.style.setProperty("--Theme1-Color5", colors[ev.target.id].color3);
+  root.style.setProperty("--Selection-Color", colors[ev.target.id].color4);
+  root.style.setProperty("--little-buttons", colors[ev.target.id].color5);
 });
 
 // Couleur du texte de la console
